6cc3b535a0ea5ba9a85a801ffa081c0318d6bf84
iloozealot <iloozealot@gmail.com>
2023-11-19 09:26:12 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using ConsoleUI;
+
+double x = RequestData.GetADouble("Please enter your first number: ");
+double y = RequestData.GetADouble("Please enter your second number: ");
+
+double resultAdd = CalculateData.Add(x, y);
+double resultSubtract = CalculateData.Subtract(x, y);
+double resultMultiply = CalculateData.Multiply(x, y);
+double resultDivide = CalculateData.Divide(x, y);
+
+Console.WriteLine($"The sum of {x} and {y} is {resultAdd}.");
+Console.WriteLine($"The subtraction of {x} and {y} is {resultSubtract}.");
+Console.WriteLine($"The multiplication of {x} and {y} is {resultMultiply}.");
+Console.WriteLine($"The divide of {x} and {y} is {resultDivide}.");
+
+Console.ReadLine();
